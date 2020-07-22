4fc9618385a334902091c8c060ed7c8920daf2d7
estherkang14 <estherkang14@gmail.com>
2020-07-22 16:05:20 -0400

    fixedgit add .
    q

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     e.preventDefault()
 
     fetchAllDogs()
+    // delegateClicks()
 })
 
 let renderDogs = (dogs) => {
@@ -12,6 +13,7 @@ let renderDogs = (dogs) => {
 
 let renderDog = (dog) => {
     let tableBody = document.getElementById('table-body')
+
     let tableRow = document.createElement('tr')
     tableRow.dataset.id = dog.id
     tableBody.appendChild(tableRow)
@@ -36,37 +38,66 @@ let renderDog = (dog) => {
     editButton.classList += 'edit'
     editButton.innerText = 'Edit Dog'
     editButton.addEventListener('click', (e) => {
-        updateDogInfo(e.target.parentNode)
+        let editForm = document.getElementById('dog-form')
+        editForm.reset()
+        editForm.name.value = dog.name
+        editForm.breed.value = dog.breed
+        editForm.sex.value = dog.sex
+        editForm.dataset.id = dog.id
+        updateDogInfo(dog, editForm)
     })
     tableRow.appendChild(editButton)
 }
 
-let updateDogInfo = (dog) => {
-    let editForm = document.getElementById('dog-form')
-    let nameInput = editForm.querySelector('input[name="name"]')
-    nameInput.value = dog.firstChild.innerText
+// this is event delegation: 
+// let delegateClicks = () => {
+//     document.addEventListener('click', e => {
+//         if (e.target.textContent === 'Edit Dog') {
+//             const dogRow = e.target.closest('tr')
+//             const name = dogRow.children[0].textContent
+//             const breed = dogRow.children[1].textContent
+//             const sex = dogRow.children[2].textContent
+
+//             const dogForm = document.querySelector('form')
+
+//         }
+//     })
+// }
+// const submitHandler = () => {
+//     document.addEventListener('submit', e => {
+//         e.preventDefault()
+
+//     })
+// }
+let updateDogInfo = (dog, editForm) => {
+    // let editForm = document.getElementById('dog-form')
+    // let nameInput = editForm.querySelector('input[name="name"]')
+    // nameInput.value = dog.name
     
-    let dogBreed = dog.querySelector('.breed')
-    let breedInput = editForm.querySelector('input[name="breed"]')
-    breedInput.value = dogBreed.innerText
+    // let dogBreed = dog.querySelector('.breed')
+    // let breedInput = editForm.querySelector('input[name="breed"]')
+    // breedInput.value = dog.breed
 
-    let dogSex = dog.querySelector('.sex')
-    let sexInput = editForm.querySelector('input[name="sex"]')
-    sexInput.value = dogSex.innerText
+    // let dogSex = dog.querySelector('.sex')
+    // let sexInput = editForm.querySelector('input[name="sex"]')
+    // sexInput.value = dog.sex
 
-    let submitButton = editForm.querySelector('input[type="submit"]')
+    
+    // let submitButton = editForm.querySelector('input[type="submit"]')
     editForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        patchRequest(editForm, dog, nameInput, breedInput, sexInput)
+        
+        patchRequest(dog)
     })
     
 }
 
-let patchRequest = (editForm, dog, nameInput, breedInput, sexInput) => {
+let patchRequest = (dog) => {
+    let editForm = document.getElementById('dog-form')
     let editDog = {
-        "name": nameInput.value,
-        "breed": breedInput.value,
-        "sex": sexInput.value
+        "name": editForm.name.value,
+        "breed": editForm.breed.value,
+        "sex": editForm.sex.value
     }
 
     let options = {
@@ -77,14 +108,13 @@ let patchRequest = (editForm, dog, nameInput, breedInput, sexInput) => {
         },
         body: JSON.stringify(editDog)
     }
-    fetch(BASEURL + dog.dataset.id, options)
+    fetch(BASEURL + editForm.dataset.id, options)
     .then(response => response.json())
     .then( dog => updateDogTable() )
 }
 
 let updateDogTable = () => {
     let dogList = document.getElementById('table-body')
-    console.log(dogList)
     removeAllListItems(dogList)
     fetchAllDogs()
 }
