d4f4c538217f04c8f5e93987f1ef3c76c536a12a
ianmcknnn <ianmcnnn@gmail.com>
2020-07-22 11:18:37 -0400

    almost all deliverables working. patch prints to console instead of editing dom because I ran out of time

--- src/index.js
+++ src/index.js
@@ -1,36 +1,91 @@
 DOGS_URL = "http://localhost:3000/dogs"
 
 document.addEventListener('DOMContentLoaded', () => {
-	
+
 	const tableBody = document.getElementById("table-body");
+	const dogForm = document.getElementById("dog-form");
+
 	function getDogs(){
 		fetch(DOGS_URL)
-		.then(resp => resp.json())
-		.then(displayDogs)
-	//	.catch(error => error.forEach(alert(error.message)))
+			.then(resp => resp.json())
+			.then(displayDogs)
+		//		.catch(error => error.forEach(alert(error.message)))
 	}
 
 	function displayDogs(json){
-		json.forEach(displayDog)
+		json.forEach(displayDog);
+		document.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const submitButton = dogForm[3];
+
+			if(submitButton.dataset['id']){
+				submitForm();
+			}
+		})
 	}
 
 	function displayDog(dog){
-		const newRow = document.createElement("tr")
-		const nameData = document.createElement("td")
-		const breedData = document.createElement("td")
-		const sexData = document.createElement("td")
-		const editButton = document.createElement("button")
-		const editButtonTd = document.createElement("td")
+//		if(!document.querySelector(`td > [data-id='${dog["id"]}']`)){
+			const newRow = document.createElement("tr")
+			const nameData = document.createElement("td")
+			const breedData = document.createElement("td")
+			const sexData = document.createElement("td")
+			const editButton = document.createElement("button")
+			const editButtonTd = document.createElement("td")
 
-		nameData.textContent = dog['name'];
-		breedData.textContent = dog['breed'];
-		sexData.textContent = dog['sex'];
-		editButton.textContent = "Edit Dog";
-		editButtonTd.append(editButton);
+			nameData.textContent = dog['name'];
+			breedData.textContent = dog['breed'];
+			sexData.textContent = dog['sex'];
+			editButton.textContent = "Edit Dog";
+			editButton.setAttribute("data-id", dog['id']);
+			editButtonTd.append(editButton);
 
-		newRow.append(nameData, breedData, sexData, editButtonTd);
-		tableBody.append(newRow);
+			newRow.append(nameData, breedData, sexData, editButtonTd);
+			tableBody.append(newRow);
+
+			editButton.addEventListener("click", (e) => {
+				dogForm.name.value = dog['name'];
+				dogForm.breed.value = dog['breed'];
+				dogForm.sex.value = dog['sex'];
+				dogForm[3].setAttribute('data-id', dog['id']);
+			})
+//		}
+//			else{
+//				const editRow = document.querySelector(`td > [data-id='${dog["id"]}']`).parentElement.parentElement;
+//			}
 	}
+
+	function submitForm() {
+		const newName = dogForm.name.value;
+		const newBreed = dogForm.breed.value;
+		const newSex = dogForm.sex.value;
+		const submitButton = dogForm[3];
+		const dogId = submitButton.dataset['id'];
+
+		if(newName == '' || newBreed == '' || newSex == ''){
+			alert('Please fill in all fields before submitting');
+		}
+		else{
+
+			PATCH_PARAMS = { method: 'PATCH',
+				headers: {
+					"Content-Type": "application/json",
+					"Accept": "application/json"
+				},
+				body: JSON.stringify({
+					name: newName,
+					breed: newBreed,
+					sex: newSex
+				})
+			};
+
+			fetch(DOGS_URL + `/${dogId}`, PATCH_PARAMS)
+				.then(resp => resp.json())
+				.then(dog => console.log(dog))
+		}
+	}
+
 
 	getDogs();
 })
