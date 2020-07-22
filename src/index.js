fc3ba30f1a4df0b9227891f98b1e81d0361b9b56
Nyelanioustour <shawn.k.alexander@gmail.com>
2020-07-22 11:02:54 -0400

    midsave

--- src/index.js
+++ src/index.js
@@ -1,3 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const URL = "http://localhost:3000/dogs"
+    let dogForm = document.getElementById('dog-form')
+    let dogInputs = dogForm.querySelectorAll('input')
+    
+    getDogs()
+    
+    dogInputs[3].addEventListener('click',event=>{
+        event.preventDefault()
+        sumbitHandler()
+    })
 
+    function sumbitHandler(){
+        // let dogForm = document.getElementById('dog-form')
+        // let dogInputs = dogForm.querySelectorAll('input')
+        let dog = {"name":dogInputs[0],"breed":dogInputs[1],"sex":dogInputs[2]}
+        console.log(dog)
+        // fetch(URL,{
+        //     method: "POST",
+        //     headers:{
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(dog)
+        // })
+        // .then(response=>response.json())
+        // .then(dog=>{
+        //     console.log(dog)
+        //     renderDog(dog)
+        // })
+    
+    }
+    
+
+
+function getDogs(){
+    fetch(URL).then(response => response.json()).then(dogs => {renderDogs(dogs)})
+}
+
+function renderDogs(dogs){
+    dogs.forEach(dog => {renderDog(dog)})
+}
+
+function renderDog(dog){
+    let tableBody = document.getElementById('table-body')
+    let tableRow = document.createElement('tr')
+    let tdName = document.createElement('td')
+    let tdBreed = document.createElement('td')
+    let tdSex = document.createElement('td')
+    let tdEdit = document.createElement('td')
+    let editButton = document.createElement('button')
+
+    tdName.textContent = dog.name
+    tdBreed.textContent = dog.breed
+    tdSex.textContent = dog.sex
+    editButton.textContent = "Edit"
+
+    tdEdit.append(editButton)
+    tableRow.append(tdName,tdBreed,tdSex,editButton,tdEdit)
+    tableBody.append(tableRow)
+
+    editButton.addEventListener('click', event =>{
+        editDogHandler(dog)
+    })
+}
+
+function editDogHandler(dog){
+    // let dogForm = document.getElementById('dog-form')
+    // let dogInputs = dogForm.querySelectorAll('input')
+    
+    dogInputs[0].value = dog.name
+    dogInputs[1].value = dog.breed
+    dogInputs[2].value = dog.sex
+    
+    // dogInputs[3].addEventListener('click', event=>{
+    //     event.preventDefault()
+    //     sumbitHandler(dog.id)
+    // })
+}
+
+        
+    
+    
 })
